ffcac463794b15270e17ee615d6b3fdae66b5e5d
LACY HOGAN <lacy.hogan@gmail.com>
2018-03-15 08:16:27 -0700

    Completed Insert Dashes

---
+++ insert-dashes/solution.js
@@ -0,0 +1,16 @@
+function insertDash(num) {
+    let numArr = num.toString().split('');
+    let flag = false;
+    for (let i=0; i < numArr.length; i++) {
+      if (numArr[i]%2===0){
+        flag = false;
+      } else if (flag === true){
+        numArr.splice(i, 0, '-');
+        i++;
+        continue;
+      } else{
+        flag = true;
+      }
+    }
+    return numArr.join('');
+  }  
